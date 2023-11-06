d3d422a6f5a7c847c1eb849d1546e5c22d5b58ec
BrianPizz <bpizzimentijr@outlook.com>
2023-11-05 21:15:32 -0800

    create thought route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 const { User, Thought } = require('../../models')
 
+// Create a thought
+router.post('/', async (req, res) => {
+    try {
+        const thought = await Thought.create(req.body);
+        const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { runValidators: true, new: true }
+        )
+        res.json(user)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+})
+
 module.exports = router;
