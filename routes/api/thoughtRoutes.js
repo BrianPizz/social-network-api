f9fb53bcd51dfaa935c1032829f1b556eccdbf06
BrianPizz <bpizzimentijr@outlook.com>
2023-11-05 21:28:27 -0800

    get thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,33 @@
 const router = require('express').Router();
 const { User, Thought } = require('../../models')
 
+// Get all thoughts
+router.get('/', async (req, res) => {
+    try {
+        const thoughts = await Thought.find();
+
+        res.json(thoughts)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+
+// Get a specific thought
+router.get('/:thoughtId', async (req, res) => {
+    try {
+        const thought = await Thought.findOne({ _id: req.params.thoughtId })
+
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought found with that ID' })
+        }
+        res.json(thought)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+
 // Create a thought
 router.post('/', async (req, res) => {
     try {
@@ -15,6 +42,7 @@ router.post('/', async (req, res) => {
         console.log(err);
         return res.status(500).json(err);
     }
-})
+});
+
 
 module.exports = router;
